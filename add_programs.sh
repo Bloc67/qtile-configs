0877074e568ac69f3a3756e39027ad0c868aba76
Bjørn Kristiansen <post@bhkristiansen.com>
2024-01-04 15:14:16 +0100

    Update add_programs.sh

--- add_programs.sh
+++ add_programs.sh
@@ -1,6 +1,6 @@
 # bash script for installing necessary programs, run with sudo
 echo "normal programs: qtile terminator micro syncthing syncthing-gtk audacious easyeffects"
-pacman -S qtile terminator micro syncthing syncthing-gtk audacious easyeffects
+pacman -S qtile terminator micro syncthing syncthing-gtk audacious easyeffects -y
 
 echo "extra programs: bashtop lxappearance rofi yay"
-pacman -S bashtop lxappearance rofi yay
+pacman -S bashtop lxappearance rofi yay -y

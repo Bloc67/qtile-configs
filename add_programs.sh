d416bc0f0f71bdbf6f0bc870161daf7937b5439c
Bjørn Kristiansen <post@bhkristiansen.com>
2024-01-04 15:37:09 +0100

    Update add_programs.sh

--- add_programs.sh
+++ add_programs.sh
@@ -5,5 +5,14 @@ echo "run them by copy and paste!"
 cp config.py ../.config/qtile/config.py
 echo "Copied over qtile config."
 echo "Restarting.."
+echo "Press any key to reboot"
+
+# Loop until a key is pressed
+while true; do
+read -rsn1 key # Read a single character silently
+if [[ -n "$key" ]]; then
+  break # Exit the loop if a key is pressed
+fi
+done
 reboot
 

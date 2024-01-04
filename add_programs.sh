f3a7c044d8e12de54b8ff66ac1b0c55887b296ac
Bjørn Kristiansen <post@bhkristiansen.com>
2024-01-04 15:33:29 +0100

    Update add_programs.sh

--- add_programs.sh
+++ add_programs.sh
@@ -1,6 +1,9 @@
 # bash script for installing necessary programs, run with sudo
-echo "normal programs: qtile terminator micro syncthing syncthing-gtk audacious easyeffects"
-pacman -S qtile terminator micro syncthing syncthing-gtk audacious easyeffects -y
+echo "yay -S bashtop lxappearance rofi xed micro"
+echo "yay -S qtile terminator micro syncthing-bin syncthing-gtk audacious easyeffects"
+echo "run them by copy and paste!"
+cp config.py ../.config/qtile/config.py
+echo "Copied over qtile config."
+echo "Restarting.."
+reboot
 
-echo "extra programs: bashtop lxappearance rofi yay"
-pacman -S bashtop lxappearance rofi yay -y

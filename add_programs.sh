f8258647355dad6032fb033f7a77732f1ac2086e
Bjørn Kristiansen <post@bhkristiansen.com>
2024-01-04 11:04:58 +0100

    Create add_programs.sh

---
+++ add_programs.sh
@@ -0,0 +1,5 @@
+# bash script for installing necessary programs, run with sudo
+echo "normal programs: qtile terminator micro syncthing syncthing-gtk audacious easyeffects"
+pacman -S qtile terminator micro syncthing syncthing-gtk audacious easyeffects
+
+echo "extra programs?"

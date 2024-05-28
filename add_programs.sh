40e26e43c345a781051fd258261bf23bcaaa3ae3
Bjørn Kristiansen <post@bhkristiansen.com>
2024-05-28 17:15:04 +0200

    Update add_programs.sh

--- add_programs.sh
+++ add_programs.sh
@@ -1,12 +1,7 @@
 # instructions for installing necessary programs
 echo "## Installing Qtile on Manjaro Cinnamon"
-echo "yay -S bashtop lxappearance rofi micro xfce4-power-manager"
-echo "yay -S qtile terminator micro syncthing-bin syncthing-gtk quodlibet easyeffects"
+echo "yay -S lxappearance rofi micro xfce4-power-manager qtile terminator syncthing-gtk quodlibet picom arandr"
 cp config.py ../.config/qtile/config.py
 echo "Copied over qtile config."
 echo "To turn off all power options:"
-echo "sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-
-## to check
-sudo systemctl status sleep.target suspend.target hibernate.target hybrid-sleep.target
-"
+echo "sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target"

562a2b881124e0acfb275473a147d34e92c26db5
Bjørn Kristiansen <post@bhkristiansen.com>
2024-05-29 00:10:29 +0200

    Update add_programs.sh

--- add_programs.sh
+++ add_programs.sh
@@ -1,6 +1,11 @@
 # instructions for installing necessary programs
 echo "## Installing Qtile on Manjaro Cinnamon"
 echo "yay -S lxappearance rofi micro xfce4-power-manager qtile terminator syncthing-gtk quodlibet picom arandr"
+echo "yay -S emby-server emby-theater qbittorrent vscodium obsidian grsync sickchill"
+echo "yay mda"
+echo "yay calf"
+echo "yay lv2-plugins"
+echo "yay dropbox"
 cp config.py ../.config/qtile/config.py
 echo "Copied over qtile config."
 echo "To turn off all power options:"

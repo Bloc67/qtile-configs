be05068eac7ae909dd47bebf09e0a5dd425685e4
Bjørn Kristiansen <post@bhkristiansen.com>
2024-07-20 12:45:02 +0200

    Final updates

--- add_programs.sh
+++ add_programs.sh
@@ -90,20 +90,6 @@ case ${answer:0:1} in
     ;;
 esac
 
-echo "${yel}Adding Chaotic rep...${end}"
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-
-echo "add to /etc/pacman.conf..."
-echo " " | sudo tee -a /etc/pacman.conf
-echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
-echo "${yel}Done.${end}"
-echo "${grn}cat /etc/pacman.conf:${end}"
-cat /etc/pacman.conf
 lxappearance
 
 

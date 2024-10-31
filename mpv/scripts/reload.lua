5ab5d3e9502129c0cd74bf106e6ad1b8b060c744
Bjørn Kristiansen <post@bhkristiansen.com>
2024-10-31 10:48:05 +0100

    Update reload.lua

--- mpv/scripts/reload.lua
+++ mpv/scripts/reload.lua
@@ -1,20 +1,6 @@
---[[
-	reload / by sibwaf / https://github.com/sibwaf/mpv-scripts
 
-	Reopens the current playing file and seeks to the same timestamp on a button press ([Shift+R] by default).
-    Useful for situations when you are watching YouTube/streams and your connection breaks for some reason.
-
-	MIT license - do whatever you want, but I'm not responsible for any possible problems.
-	Please keep the URL to the original repository. Thanks!
-]]
-
-function reload()
-    local path = mp.get_property("path")
-    if path ~= nil then
-        local time = mp.get_property_number("time-pos")
-        mp.osd_message("Reloading...")
-        mp.commandv("loadfile", path, "replace", "start=" .. time)
-    end
+function sreload()
+    mp.osd_message("Reloading subtitle...")
+    mp.commandv("sub-reload")
 end
-
-mp.add_key_binding("R", "reload", reload)
+mp.add_key_binding("R", "Subtitle reload", sreload)
